edd0f3fa8d4871d78449518bc19b6e9fc7f4e344
mulhern <amulhern@redhat.com>
2015-05-28 18:20:57 -0400

    Initial commit.
    Signed-off-by: mulhern <amulhern@redhat.com>

---
+++ examples/complex-shapes.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname complex-shapes) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")))))
+(define my-star (star 64 "solid" "red"))
+
+(define my-circle (circle 64 "solid" "blue"))
